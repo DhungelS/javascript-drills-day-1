da7e90fe4a095466bf207a64009566188bc22b67
Brian Dudey <dudey.brian@gmail.com>
2018-01-15 13:44:00 -0600

    fourth commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function whoAmI(name, age) {
+    var yearOfBirth = 2018 - age;
     console.log(`I'm ${name} and I am ${age}`)
+    console.log(`I was born in ${yearOfBirth}`)
 }
 whoAmI("Superman", 15);
