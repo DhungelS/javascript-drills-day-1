208a44102fb8331af82ddb841b5e23465491194d
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-15 15:16:57 -0500

    fifth commit

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
+function yearOfBirth(age){
+  try{
+   
+  var yearOfBirth = 2018 - age;
+  if(age < 0){
+    throw "Age cannot be less than 0"
+    }
+      console.log(`I was born in ${yearOfBirth}`)
+    
+  }
+  catch(e) {
+      console.log(e);
+    }
+  }
+
+
 function whoAmI(name, age) {
-    var yearOfBirth = 2018 - age;
+  
+  if(name !== undefined || age !== undefined){
     console.log(`I'm ${name} and I am ${age}`)
-    console.log(`I was born in ${yearOfBirth}`)
+   return yearOfBirth(age);
+  }
+    
+  else {
+    console.log("Arguments are not valid")
+    }
+
 }
-whoAmI("Superman", 15);
+//whoAmI("Superman", 15);
+whoAmI()
