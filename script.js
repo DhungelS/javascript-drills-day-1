b8ebd477df0221918f201f513580e0a1974b7de4
Brian Dudey <dudey.brian@gmail.com>
2018-01-15 14:30:10 -0600

    Sixth Commit

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-function yearOfBirth(age){
-  try{
-   
-  var yearOfBirth = 2018 - age;
-  if(age < 0){
-    throw "Age cannot be less than 0"
+function yearOfBirth(age) {
+    try {
+
+        var yearOfBirth = 2018 - age;
+        if (age < 0) {
+            throw "Age cannot be less than 0"
+        }
+        console.log(`I was born in ${yearOfBirth}`)
+
+    } catch (e) {
+        console.log(e);
     }
-      console.log(`I was born in ${yearOfBirth}`)
-    
-  }
-  catch(e) {
-      console.log(e);
-    }
-  }
+}
 
 
 function whoAmI(name, age) {
-  
-  if(name !== undefined || age !== undefined){
-    console.log(`I'm ${name} and I am ${age}`)
-   return yearOfBirth(age);
-  }
-    
-  else {
-    console.log("Arguments are not valid")
+
+    if (name !== undefined || age !== undefined) {
+
+        if (age === NaN || name !== "") {
+            console.log("'Age' must be a number and 'Name' must be a string")
+
+            console.log(`I'm ${name} and I am ${age}`)
+            return yearOfBirth(age);
+        }
+    } else {
+        console.log("Arguments are not valid")
     }
+
 
 }
 //whoAmI("Superman", 15);
-whoAmI()
+whoAmI("Brian", 37)
